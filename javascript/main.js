a7301ed629f3f06f02b7758d1701b07372d02456
Yealin Park <Yealin@Yealinui-MacBook-Pro.local>
2017-08-09 17:02:15 +0900

    semi-final

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,21 @@
 $( document ).ready(function() {
 
+	console.log("wow");
+
+
+
 	var currenturl = window.location.href;
+	var a = "photos.html"
+	var b = currenturl.indexOf(a)
+
+	console.log(b)
+
+	if(b!=-1){$("#gallery").unitegallery();}
+
+	
+
+	
+
 	console.log("current URL is: " + currenturl);
 
 	var i = currenturl.indexOf("github.io");
@@ -8,19 +23,34 @@ $( document ).ready(function() {
 	console.log(i);
 
 	i=i+9;
-
 	var u = currenturl.substring(i)
 
 	console.log('c'+u);
 
 	$('.greywhenhover').each(function(i, obj) {
+		console.log("check")
 	    if ($(this).attr('href').indexOf(u)!=-1){
 			$(this).css('color','#FF7F50');
 	    }
 	});
 
-
-
+	function init() {
+		console.log("hello")
+	    window.addEventListener('scroll', function(e){
+	        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+	            shrinkOn = 50,
+	            header = document.querySelector("header");
+	        if (distanceY > shrinkOn) {
+	            classie.add(header,"smaller");
+	        } else {
+	            if (classie.has(header,"smaller")) {
+	                classie.remove(header,"smaller");
+	            }
+	        }
+	    });
+	}
+	init();
+        
 	console.log("javascript done");
 });
 
